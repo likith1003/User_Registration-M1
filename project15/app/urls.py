ee82ec1893ef841436e7a1e2734ecd133a404406
likith1003 <123715227+likith1003@users.noreply.github.com>
2024-08-17 11:30:52 +0530

    Add files via upload

--- project15/app/urls.py
+++ project15/app/urls.py
@@ -3,5 +3,9 @@ from .views import *
 
 
 urlpatterns = [
-    path('register', register, name='register')
+    path('register', register, name='register'),
+    path('user_login', user_login, name='user_login'),
+    path('home', home, name='home'),
+    path('user_profile', user_profile, name='user_profile'),
+    path('user_logout', user_logout, name='user_logout')
 ]
--- project15/app/views.py
+++ project15/app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from .forms import *
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.mail import send_mail
+from django.contrib.auth import authenticate,login, logout
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def register(request):
@@ -18,7 +22,53 @@ def register(request):
             MPFDO = PFDO.save(commit=False)
             MPFDO.username=MUFDO
             MPFDO.save()
-            return HttpResponse('Done.....')
+            message = f"Hello {UFDO.cleaned_data.get('first_name')}\n \t Your Registration against our application is successfully Done with username:{UFDO.cleaned_data.get('username')} \n\n \t\t Thanks & regards \n \t\tTeam"
+            email = UFDO.cleaned_data.get('email')
+            # send_mail(
+            #     'Registration Successfull',
+            #     message,
+            #     'likith.qsp@gmail.com',
+            #     [email],
+            #     fail_silently=False
+            # )
+            return HttpResponseRedirect(reverse('user_login'))
         return HttpResponse('Invalid Data')
-
     return render(request, 'register.html', d)
+
+
+def user_login(request):
+    if request.method == 'POST':
+        un = request.POST.get('un')
+        pw = request.POST.get('pw')
+        AUO = authenticate(username=un, password=pw)
+        if AUO:
+            login(request, AUO)
+            request.session['username'] = un
+            return HttpResponseRedirect(reverse('home'))
+        return HttpResponse('Invalid Credentials')
+    return render(request, 'user_login.html')
+
+def home(request):
+    un = request.session.get('username')
+    if un:
+        UO = User.objects.get(username=un)
+        d = {'UO': UO}
+        return render(request, 'home.html', d)
+    return render(request, 'home.html')
+
+
+@login_required
+def user_profile(request):
+    un = request.session.get('username')
+    if un:
+        UO = User.objects.get(username = un)
+        PO = Profile.objects.get(username=UO)
+        d = {'UO': UO, 'PO': PO}
+        return render(request, 'user_profile.html', d)
+    return render(request, 'user_profile.html')
+
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('home'))
